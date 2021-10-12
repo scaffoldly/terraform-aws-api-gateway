50ea1fde77c20d65dc17a72459c4937be0fee568
Christian Nuss <cnuss@users.noreply.github.com>
2021-10-12 07:51:06 -0700

    scaffoldly/api-gateway-domain/aws 1.0.9

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_api_gateway_account" "account" {
 
 module "domain" {
   source  = "scaffoldly/api-gateway-domain/aws"
-  version = "1.0.8"
+  version = "1.0.9"
 
   for_each = var.stages
 
